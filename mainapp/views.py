ffd21deb05de845c4b838cc2078414fc657825da
Max <shalyapapro@yandex.ru>
2022-04-05 20:09:44 +0300

    created patient_list

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from mainapp.models import Patient
+
+
+class PatientView(ListView):
+    template_name = 'mainapp/patients_list.html'
+    context_object_name = 'patients'
+
+    def get_queryset(self):
+        return Patient.objects.all()
